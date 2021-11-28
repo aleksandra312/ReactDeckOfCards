323ef2aa9a4a09eff60d04a627e30c2f2e27573a
aleksandra <oleksandra.klevchuk@gmail.com>
2021-11-27 22:31:48 -0600

    add functionality to shuffle cards.

--- src/DeckOfCards.js
+++ src/DeckOfCards.js
@@ -1,26 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card";
 import { v4 as uuid } from "uuid";
 import axios from "axios";
+import "./DeckOfCards.css";
+
+const BASE_URL = "http://deckofcardsapi.com/api/deck";
 
 const DeckOfCards = () => {
+  const deckId = useRef();
   const [cards, setCards] = useState([]);
 
   const addCard = (newCard) => {
     setCards((cards) => [...cards, { ...newCard, id: uuid() }]);
   };
 
-  async function drawCard() {
-    const res = await axios.get(
-      "http://deckofcardsapi.com/api/deck/swr2iokx9s2f/draw/?count=1"
-    );
+  useEffect(() => {
+    async function shuffleDeck() {
+      const res = await axios.get(`${BASE_URL}/new/shuffle/?deck_count=1`);
+      deckId.current = res.data.deck_id;
+    }
+    shuffleDeck();
+  }, []);
 
-    const cardImg = res.data.cards[0].image;
-    addCard({ image: `${cardImg}`, id: uuid() });
+  async function drawCard() {
+    const res = await axios.get(`${BASE_URL}/${deckId.current}/draw/?count=1`);
+    addCard({ image: `${res.data.cards[0].image}`, id: uuid() });
+    if (res.data.remaining === 0) {
+      alert("Error: no cards remaining!");
+    }
   }
 
   return (
-    <div>
+    <div className="DeckOfCards">
       <button onClick={drawCard}>GIMME A CARD!</button>
       <div>
         {cards.map(({ image, id }) => (
